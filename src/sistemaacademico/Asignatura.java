8d192395b61f956fc3ac54ff1868ed5571c7b9ca
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 08:54:49 -0500

    feat: agregar toString a clase Asignatura

--- src/sistemaacademico/Asignatura.java
+++ src/sistemaacademico/Asignatura.java
@@ -50,4 +50,12 @@ public class Asignatura {
     public void setDocente(String docente) {
         this.docente = docente;
     }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" + "codigo=" + codigo + ", nombre=" + nombre + ", creditos=" + creditos + ", docente=" + docente + '}';
+    }
+    
+    
+    
 }
