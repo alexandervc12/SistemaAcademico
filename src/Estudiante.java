d16e36e74fb202b43f032d9f176e4e5148824bea
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 08:44:14 -0500

    feat: crear clase Estudiante con atributos

---
+++ src/Estudiante.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package sistemaacademico;
+
+public class Estudiante {
+    private String codigo;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private int semestre;
+}
