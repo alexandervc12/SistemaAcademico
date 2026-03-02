863e664e3c7f1b0b619b0717b5f39aa09cc46d0d
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 08:55:50 -0500

    feat: crear clase Nota con atributos

---
+++ src/sistemaacademico/Nota.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+public class Nota {
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;
+    
+    
+}
