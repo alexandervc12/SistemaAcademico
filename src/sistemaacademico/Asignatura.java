fcfb5c7f195f5d3f53c168e5794d4854256a1010
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 08:52:06 -0500

    feat: crear clase Asignatura con atributos

---
+++ src/sistemaacademico/Asignatura.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
+}
