3585a9d11984d6a593c863f9ee495cdb7f34d5ba
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 09:10:13 -0500

    feat: crear estructura base de Main con ArrayList

---
+++ src/sistemaacademico/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaacademico;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    private static ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    private static ArrayList<Nota> notas = new ArrayList<>();
+    private static Scanner leer = new Scanner(System.in);
+    
+}
