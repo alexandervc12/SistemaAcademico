7888334f6fbc24b2c15cdf5d69e743226e1e78a3
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 08:54:01 -0500

    feat: agregar encapsulamiento a clase Asignatura

--- src/sistemaacademico/Asignatura.java
+++ src/sistemaacademico/Asignatura.java
@@ -4,10 +4,50 @@
  */
 package sistemaacademico;
 
-
 public class Asignatura {
     private String codigo;
     private String nombre;
     private int creditos;
     private String docente;
+    
+    // Constructor
+    public Asignatura(String codigo, String nombre, int creditos, String docente) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.docente = docente;
+    }
+    
+    // Getters y Setters
+    public String getCodigo() {
+        return codigo;
+    }
+    
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public int getCreditos() {
+        return creditos;
+    }
+    
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+    
+    public String getDocente() {
+        return docente;
+    }
+    
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
 }
