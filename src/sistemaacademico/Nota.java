fb1b61e1e6eb4d8032cd15490cf0652d049277e4
alexander vesga <avesga@unicesar.edu.co>
2026-03-02 08:56:48 -0500

    feat: agregar encapsulamiento a clase Nota

--- src/sistemaacademico/Nota.java
+++ src/sistemaacademico/Nota.java
@@ -10,5 +10,44 @@ public class Nota {
     private double valor;
     private String periodo;
     
+    // Constructor
+    public Nota(Estudiante estudiante, Asignatura asignatura, double valor, String periodo) {
+        this.estudiante = estudiante;
+        this.asignatura = asignatura;
+        this.valor = valor;
+        this.periodo = periodo;
+    }
     
+    // Getters y Setters
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+    
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+    
+    public Asignatura getAsignatura() {
+        return asignatura;
+    }
+    
+    public void setAsignatura(Asignatura asignatura) {
+        this.asignatura = asignatura;
+    }
+    
+    public double getValor() {
+        return valor;
+    }
+    
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public String getPeriodo() {
+        return periodo;
+    }
+    
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
 }
